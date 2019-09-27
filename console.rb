95d2945c067874034378304a96060f9fe76ccf42
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-09-27 21:31:18 +0100

    film save method created and tested

--- console.rb
+++ console.rb
@@ -1,5 +1,5 @@
 require_relative('models/customer')
-# require_relative('models/film')
+require_relative('models/film')
 # require_relative('models/ticket')
 
 require('pry')
@@ -9,6 +9,12 @@ customer2 = Customer.new({'name' => 'Avid Goggler', 'funds' => 10})
 
 customer1.save()
 customer2.save()
+
+film1 = Film.new({'title' => 'The Weeping Meadow Strikes Again', 'price' => 12})
+film2 = Film.new({'title' => 'Star Wars Episode XXXVIII: May The Fans Be With Us', 'price' => 6})
+
+film1.save()
+film2.save()
 
 binding.pry
 nil
