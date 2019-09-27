96526050218d7525e73f3546490e360766c13314
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-09-27 21:59:11 +0100

    ticket save method created and tested

--- console.rb
+++ console.rb
@@ -1,11 +1,11 @@
 require_relative('models/customer')
 require_relative('models/film')
-# require_relative('models/ticket')
+require_relative('models/ticket')
 
 require('pry')
 
 customer1 = Customer.new({'name' => 'Sinnie File', 'funds' => 100})
-customer2 = Customer.new({'name' => 'Avid Goggler', 'funds' => 10})
+customer2 = Customer.new({'name' => 'Avid Goggler', 'funds' => 50})
 
 customer1.save()
 customer2.save()
@@ -15,6 +15,14 @@ film2 = Film.new({'title' => 'Star Wars Episode XXXVIII: May The Fans Be With Us
 
 film1.save()
 film2.save()
+
+ticket1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket2 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film1.id})
+ticket3 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film2.id})
+
+ticket1.save
+ticket2.save
+ticket3.save
 
 binding.pry
 nil
---
+++ models/ticket.rb
@@ -0,0 +1,23 @@
+require_relative('customer')
+require_relative('film')
+require_relative('../db/sql_runner')
+
+class Ticket
+
+  attr_reader :customer_id, :film_id, :id
+
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @customer_id = options['customer_id'].to_i()
+    @film_id = options['film_id'].to_i()
+  end
+
+  def save()
+    sql = "INSERT INTO tickets (customer_id, film_id)
+    VALUES ($1, $2)
+    RETURNING id"
+    values = [@customer_id, @film_id]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i()
+  end
+
+end
